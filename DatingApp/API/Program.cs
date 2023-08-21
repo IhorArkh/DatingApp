4a2fa459924b99a562a102e4f5391e840a8afead
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-08-21 22:52:48 +0300

    Adding HTTPS for client using mkcert

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -36,7 +36,7 @@ namespace API
 
             app.UseAuthorization();
 
-            app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.MapControllers();
 
             app.Run();
