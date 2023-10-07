209e2edf0f02258006d2b384183b5f1958996656
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-10-08 00:18:09 +0300

    Exception handling middleware ready.

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 namespace API
 {
@@ -21,6 +22,8 @@ namespace API
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
