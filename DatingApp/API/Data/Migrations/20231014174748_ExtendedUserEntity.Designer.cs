f140a73b0030ccaecfb33b12f0f4cd75372ef169
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-10-14 21:10:31 +0300

    Users seeded in the Db

--- DatingApp/API/Data/Migrations/20231014174748_ExtendedUserEntity.Designer.cs
+++ DatingApp/API/Data/Migrations/20231014174748_ExtendedUserEntity.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231013212726_ExtendedUserEntity")]
+    [Migration("20231014174748_ExtendedUserEntity")]
     partial class ExtendedUserEntity
     {
         /// <inheritdoc />
@@ -82,8 +82,8 @@ namespace API.Data.Migrations
                     b.Property<bool>("IsMain")
                         .HasColumnType("INTEGER");
 
-                    b.Property<int>("PublicId")
-                        .HasColumnType("INTEGER");
+                    b.Property<string>("PublicId")
+                        .HasColumnType("TEXT");
 
                     b.Property<string>("Url")
                         .HasColumnType("TEXT");
--- DatingApp/API/Entities/Photo.cs
+++ DatingApp/API/Entities/Photo.cs
@@ -11,7 +11,7 @@ public class Photo
 
     public bool IsMain { get; set; }
 
-    public int PublicId { get; set; }
+    public string PublicId { get; set; }
 
     public int AppUserId { get; set; }
 
